6fd69536eb3c22ce84b2cf390c558f0faad051ce
Zac Pompa <git@bustachimes.com>
2021-06-13 23:43:45 -0500

    player controller mostly work
    everything but the wall sliding is working currently. Also need to fix the run animation. It doesn't stop when the key is unpressed. Only when the key is unpressed and then it plays till the animation is over.

--- whiteBoxYokai/Assets/Scripts/PlayerController.cs
+++ whiteBoxYokai/Assets/Scripts/PlayerController.cs
@@ -4,32 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private int amountOfJumpsLeft;
+
     private float movementInputDirection;
 
     private bool isFacingRight = true;
     private bool isRunning;
     private bool isGrounded;
+    private bool isTouchingWall;
+    private bool isWallSliding;
+    private bool canJump;
 
     private Rigidbody2D rb;
     private Animator anim;
 
+    public int amountOfJumps = 1;
+
     public float movementSpeed = 10.0f;
     public float jumpForce = 16.0f;
     public float groundCheckRadius;
+    public float wallCheckDistance;
+    public float wallSlideSpeed;
 
     public Transform groundCheck;
+    public Transform wallCheck;
+
 
     public LayerMask whatIsGround;
 
-    void Start() {
+    // Start is called before the first frame update
+    void Start() { 
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
     }
 
-    void Update() { // called once per frame
+    // Update is called once per frame
+    void Update() { 
         CheckInput();
         CheckMovementDiretion();
         UpdateAnimations();
+        CheckIfCanJump();
+        CheckIfWallSliding();
     }
 
     private void FixedUpdate() {
@@ -37,8 +53,31 @@ public class PlayerController : MonoBehaviour
         CheckSurroundings();
     }
 
+    private void CheckIfWallSliding() {
+        if (isTouchingWall && !isGrounded && rb.velocity.y < 0) {
+            isWallSliding = true;
+        }
+        else {
+            isWallSliding = false;
+        }
+    }
+
     private void CheckSurroundings() {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+    }
+
+    private void CheckIfCanJump() {
+        if (isGrounded == true) {
+            amountOfJumpsLeft = amountOfJumps;
+        }
+        
+        if (amountOfJumpsLeft <= 0) {
+            canJump = false;
+        }
+        else {
+            canJump = true;
+        }
     }
 
     private void CheckMovementDiretion() {
@@ -71,11 +110,20 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Jump() {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (canJump) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+        }
     }
 
     private void ApplyMovement() {
         rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+
+        if(isWallSliding) {
+            if(rb.velocity.y < -wallSlideSpeed) {
+                rb.velocity = new Vector2(rb.velocity.x, -wallSlideSpeed);
+            }
+        }
     }
 
     private void Flip() {
@@ -86,5 +134,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnDrawGizmos() {
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance,
+            wallCheck.position.y, wallCheck.position.z));
     }
 }
